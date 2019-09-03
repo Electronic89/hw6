7334896fd1367d35aac4ee9fc07b8515eda27e75
Евгений <e_kharionovskiy@rambler.ru>
2019-09-03 15:53:48 +0300

    1. Создать и запустить программу для построения двоичного дерева. В цикле построить двадцать деревьев с глубиной в 6 уровней. Данные, которыми необходимо заполнить узлы деревьев, представляются в виде чисел типа int. Число, которое попадает в узел, должно генерироваться случайным образом в диапазоне от -100 до 100. 2. Проанализировать, какой процент созданных деревьев являются несбалансированными.

---
+++ Tree.java
@@ -0,0 +1,28 @@
+public interface Tree<E> {
+
+    enum TraverseMode {
+        IN_ORDER,
+        PRE_ORDER,
+        POST_ORDER,
+    }
+
+    boolean add(E value);
+
+    boolean find(E value);
+
+    boolean remove(E value);
+
+    boolean isEmpty();
+
+    int size();
+
+    void traverse(TraverseMode mode);
+
+    void display();
+
+    boolean isBalanced();
+
+    Node getRoot();
+
+    int height();
+}
